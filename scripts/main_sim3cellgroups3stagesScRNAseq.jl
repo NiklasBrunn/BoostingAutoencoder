7181ec1877f63a1b8c2ac2835258b44c31af6a1f
NiklasBrunn <74356533+NiklasBrunn@users.noreply.github.com>
2024-04-10 20:10:02 +0200

    Update main_sim3cellgroups3stagesScRNAseq.jl

--- scripts/main_sim3cellgroups3stagesScRNAseq.jl
+++ scripts/main_sim3cellgroups3stagesScRNAseq.jl
@@ -134,7 +134,7 @@ B = trainBAE(L, BAE; mode=mode, time_series=true, zdim=zdim, ϵ=ϵ, batchsize=ba
 B_perm = zeros(size(B));
 for dim in 1:zdim
     for t in 1:length(L)
-        B_perm[:, zdim*(dim-1)+t] = B[:, (t-1)*zdim+dim]
+        B_perm[:, length(L)*(dim-1)+t] = B[:, (t-1)*zdim+dim]
     end
 end
 
